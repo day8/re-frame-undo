3598fddd8c590f9be2b6cb1537953e937ea7a79e
Stuart Mitchell <stu@stuartmitchell.com>
2016-07-25 14:16:45 +1200

    added a test for the undoable middleware

--- test/day8/re_frame/undo/undo_test.cljs
+++ test/day8/re_frame/undo/undo_test.cljs
@@ -7,6 +7,7 @@
 
 
 (deftest test-undos
+  (reset! db/app-db {})
   ;; Create undo history
   (undo/undo-config! {:max-undos 5})
 
@@ -45,6 +46,61 @@
   (is (not (undo/redos?)))
   (is (undo/undos?))
   (is (= [{5 5} {6 6} {7 7} {8 8} {9 9}] @undo/undo-list))
+
+  ;; Clear history
+  (undo/clear-history!)
+  (is (not (undo/undos?)))
+  (is (not (undo/redos?))))
+
+
+(deftest test-undos-middleware
+  (reset! db/app-db {})
+  ;; Create undo history
+  (undo/undo-config! {:max-undos 5})
+
+  (undo/clear-history!)
+  (is (not (undo/undos?)))
+  (is (not (undo/redos?)))
+
+  (re-frame/reg-event
+    :change-db
+    (undo/undoable "change-db")
+    (fn [db _]
+      (update db :test inc)))
+
+  (doseq [i (range 10)]
+    (re-frame/dispatch-sync [:change-db]))
+
+  ;; Check the undo state is correct
+  (is (undo/undos?))
+  (is (not (undo/redos?)))
+  (is (= ["change-db" "change-db" "change-db" "change-db" "change-db" "change-db"]
+         (undo/undo-explanations)))
+  (is (= [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]
+         @undo/undo-list))
+
+  ;; Undo the actions
+  (re-frame/dispatch-sync [:undo])
+  (is (= @db/app-db {:test 9}))
+  (is (undo/redos?))
+  (re-frame/dispatch-sync [:undo])
+  (is (= @db/app-db {:test 8}))
+  (re-frame/dispatch-sync [:undo])
+  (is (= @db/app-db {:test 7}))
+  (re-frame/dispatch-sync [:undo])
+  (is (= @db/app-db {:test 6}))
+  (re-frame/dispatch-sync [:undo])
+  (is (= @db/app-db {:test 5}))
+  (is (not (undo/undos?)))
+  (is (undo/redos?))
+
+  ;; Redo them again
+  (re-frame/dispatch-sync [:redo 5])
+  (is (= @db/app-db {:test 10}))
+  (is (not (undo/redos?)))
+  (is (undo/undos?))
+  (is (= [{:test 5} {:test 6} {:test 7} {:test 8} {:test 9}]
+         @undo/undo-list))
 
   ;; Clear history
   (undo/clear-history!)
