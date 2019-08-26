9a5e9f884b7cbc39dc09ecfafc274a2de7c91fa4
Isaac Johnston <isaac@superstruct.nz>
2019-08-26 18:46:39 +0300

    Migrate to shadow-cljs

--- project.clj
+++ project.clj
@@ -1,19 +1,22 @@
 (defproject day8.re-frame/undo "0.3.3-SNAPSHOT"
   :description  "A library which provides undo/redo facility for re-frame"
   :license      {:name "MIT"}
-  :dependencies [[org.clojure/clojure        "1.8.0"]
-                 [org.clojure/clojurescript  "1.9.89"]
-                 [re-frame                   "0.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"
+                  :exclusions [com.google.javascript/closure-compiler-unshaded
+                               org.clojure/google-closure-library]]
+                 [thheller/shadow-cljs "2.8.51" :scope "provided"]
+                 [re-frame "0.10.9" :scope "provided"]]
+
+  :plugins [[lein-shadow "0.1.5"]]
 
   :profiles {:debug {:debug true}
-             :dev   {:dependencies [[karma-reporter     "1.0.1"]
-                                    [binaryage/devtools "0.8.1"]]
-                     :plugins      [[lein-ancient       "0.6.10"]
-                                    [lein-cljsbuild     "1.1.4"]
-                                    [lein-npm           "0.6.2"]
+             :dev   {:dependencies [[binaryage/devtools "0.9.10"]]
+                     :plugins      [[lein-ancient       "0.6.15"]
                                     [lein-shell         "0.5.0"]]}}
 
-  :clean-targets  [:target-path "run/compiled"]
+  :clean-targets  [:target-path
+                   "resources/public/js/test"]
 
   :resource-paths ["run/resources"]
   :jvm-opts       ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]
@@ -38,29 +41,26 @@
                   ["vcs" "commit"]
                   ["vcs" "push"]]
 
-  :npm {:dependencies [[karma                 "1.0.0"]
-                       [karma-cljs-test       "0.1.0"]
-                       [karma-chrome-launcher "0.2.0"]
-                       [karma-junit-reporter  "0.3.8"]]}
+  :shadow-cljs {:nrepl  {:port 8777}
 
-  :cljsbuild {:builds [{:id           "test"
-                        :source-paths ["test" "src"]
-                        :compiler     {:preloads      [devtools.preload]
-                                       :output-to     "run/compiled/browser/test.js"
-                                       :source-map    true
-                                       :output-dir    "run/compiled/browser/test"
-                                       :optimizations :none
-                                       :source-map-timestamp true
-                                       :pretty-print  true}}
-                       {:id           "karma"
-                        :source-paths ["test" "src"]
-                        :compiler     {:output-to     "run/compiled/karma/test.js"
-                                       :source-map    "run/compiled/karma/test.js.map"
-                                       :output-dir    "run/compiled/karma/test"
-                                       :optimizations :whitespace
-                                       :main          "re_frame_undo.test_runner"
-                                       :pretty-print  true}}]}
+                :builds {:browser-test
+                         {:target    :browser-test
+                          :ns-regexp "-test$"
+                          :test-dir  "resources/public/js/test"
+                          :compiler-options {:pretty-print true}
+                          :devtools  {:http-root "resources/public/js/test"
+                                      :http-port 8290}}
 
-  :aliases {"test-once"   ["do" "clean," "cljsbuild" "once" "test," "shell" "open" "test/test.html"]
-            "test-auto"   ["do" "clean," "cljsbuild" "auto" "test,"]
-            "karma-once"  ["do" "clean," "cljsbuild" "once" "karma,"]})
+                         :karma-test
+                         {:target    :karma
+                          :ns-regexp "-test$"
+                          :compiler-options {:pretty-print true}
+                          :output-to "target/karma-test.js"}}}
+
+  :aliases {"test-auto"   ["do"
+                           ["clean"]
+                           ["shadow" "watch" "browser-test"]]
+            "karma-once"  ["do"
+                           ["clean"]
+                           ["shadow" "compile" "karma-test"]
+                           ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]})
