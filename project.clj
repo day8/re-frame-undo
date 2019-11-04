fe6cc2763d42d17c8e818ef7c45ee2be0233fb4f
Isaac Johnston <isaac@superstruct.nz>
2019-11-04 11:12:02 +0800

    Reformat project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject day8.re-frame/undo "see :git-version below https://github.com/arrdem/lein-git-version"
+(defproject     day8.re-frame/undo "see :git-version below https://github.com/arrdem/lein-git-version"
   :description  "A library which provides undo/redo facility for re-frame"
   :license      {:name "MIT"}
 
@@ -14,15 +14,15 @@
              patch+           (inc patch)]
          (format "%s.%d-%s-SNAPSHOT" prefix patch+ ahead))))}
 
-  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+  :dependencies [[org.clojure/clojure       "1.10.1" :scope "provided"]
                  [org.clojure/clojurescript "1.10.520" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
                                org.clojure/google-closure-library]]
-                 [thheller/shadow-cljs "2.8.69" :scope "provided"]
-                 [re-frame "0.10.9" :scope "provided"]]
+                 [thheller/shadow-cljs      "2.8.69" :scope "provided"]
+                 [re-frame                  "0.10.9" :scope "provided"]]
 
   :plugins [[me.arrdem/lein-git-version "2.0.3"]
-            [lein-shadow "0.1.6"]]
+            [lein-shadow                "0.1.6"]]
 
   :profiles {:debug {:debug true}
              :dev   {:dependencies [[binaryage/devtools "0.9.10"]]
