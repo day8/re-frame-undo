ac3c12f3661c56fb7fc8336ad56648009a8f1f8f
Isaac Johnston <isaac@superstruct.nz>
2019-11-04 11:06:26 +0800

    Migrate to me.arrdem/lein-git-version

--- project.clj
+++ project.clj
@@ -1,6 +1,19 @@
-(defproject day8.re-frame/undo "0.3.4-SNAPSHOT"
+(defproject day8.re-frame/undo "see :git-version below https://github.com/arrdem/lein-git-version"
   :description  "A library which provides undo/redo facility for re-frame"
   :license      {:name "MIT"}
+
+  :git-version
+  {:status-to-version
+   (fn [{:keys [tag version branch ahead ahead? dirty?] :as git}]
+     (assert (re-find #"\d+\.\d+\.\d+" tag)
+       "Tag is assumed to be a raw SemVer version")
+     (if (and tag (not ahead?) (not dirty?))
+       tag
+       (let [[_ prefix patch] (re-find #"(\d+\.\d+)\.(\d+)" tag)
+             patch            (Long/parseLong patch)
+             patch+           (inc patch)]
+         (format "%s.%d-%s-SNAPSHOT" prefix patch+ ahead))))}
+
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                  [org.clojure/clojurescript "1.10.520" :scope "provided"
                   :exclusions [com.google.javascript/closure-compiler-unshaded
@@ -8,7 +21,8 @@
                  [thheller/shadow-cljs "2.8.67" :scope "provided"]
                  [re-frame "0.10.9" :scope "provided"]]
 
-  :plugins [[lein-shadow "0.1.6"]]
+  :plugins [[me.arrdem/lein-git-version "2.0.3"]
+            [lein-shadow "0.1.6"]]
 
   :profiles {:debug {:debug true}
              :dev   {:dependencies [[binaryage/devtools "0.9.10"]]
@@ -32,14 +46,7 @@
                                     :username :env/CLOJARS_USERNAME
                                     :password :env/CLOJARS_PASSWORD}]]
   
-  :release-tasks [["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag" "v" "--no-sign"]
-                  ["deploy" "clojars"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "push"]]
+  :release-tasks [["deploy" "clojars"]]
 
   :shadow-cljs {:nrepl  {:port 8777}
 
